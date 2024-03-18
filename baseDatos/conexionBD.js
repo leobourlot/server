b6b8b1ec2151f3c5a7afa99c9b1dbeefb795bf85
leobourlot <142550143+leobourlot@users.noreply.github.com>
2024-03-18 20:18:27 -0300

    Update conexionBD.js

--- baseDatos/conexionBD.js
+++ baseDatos/conexionBD.js
@@ -6,8 +6,8 @@ const mysql = require('mysql2/promise');
 // conexión a la base de datos
 const conexion = mysql.createPool({
     host: 'localhost',
-    user: 'leobourlot',
-    database: 'ajpp',
+    user: 'u144219150_leobourlot',
+    database: 'u144219150_ajpp',
     password: 'EDdV3C*0Aa3;'
 });
 
