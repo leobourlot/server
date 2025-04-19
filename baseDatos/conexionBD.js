d8462c0c9497948574f0ca2801958cb6cd29dd22
leobourlot <leobourlot@gmail.com>
2025-04-19 15:26:20 -0300

    conexion bd

--- baseDatos/conexionBD.js
+++ baseDatos/conexionBD.js
@@ -8,7 +8,12 @@ const conexion = mysql.createPool({
     host: 'srv1198.hstgr.io',
     user: 'u144219150_leobourlot',
     database: 'u144219150_ajpp',
-    password: 'EDdV3C*0Aa3;'
+    password: 'EDdV3C*0Aa3;',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    enableKeepAlive : true,  // <— esto ayuda a que no muera la conexión por inactividad
+    keepAliveInitialDelay: 0
 });
 
 module.exports = conexion
