9d9170a43727bdcb7a02cf44f7f1cc55bff8abb7
leobourlot <leobourlot@gmail.com>
2025-04-29 15:44:40 -0300

    pdf

--- controladores/subirArchivo.js
+++ controladores/subirArchivo.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage, limits: { fileSize: 10 * 1024 * 1024 } });
 
 exports.upload = upload.single('foto');
 
